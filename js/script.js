66221d6f537fa295a7f593ee59e273e8725d85f3
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-07-16 21:29:33 +0530

    About Us animation done along with responsiveness

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-
-
 particlesJS("particles-js", particleConfig);
 
 const linesOfCode = [
   'package org.rkmvcc.nirdesh',
   'public class <span class="text-gray-200">Nirdesh {</span>',
-  '    public static void <span class="text-blue-500">main</span><span class="text-gray-200">(</span>String<span class="text-gray-200">[] args) {</span>',
+  '   public static void <span class="text-blue-500">main</span><span class="text-gray-200">(</span>String<span class="text-gray-200">[] args) {</span>',
   '       //Get Started for the most enthusiastic coding competition',
   '       //Initiating Nirdesh',
   '       <span class="text-yellow-700">String</span> organiser =  <span class="text-green-700">"Ramakrishna Mission Vivekananda Centenary College"</span>;',
-  '       <span class="text-yellow-700">System</span>.out.println(<span class="text-green-700">"Organised by \\n"</span> + organiser);'
+  '       <span class="text-yellow-700">System</span>.out.println(<span class="text-green-700">"Organised by \\n"</span> + organiser);',
+  '    }',
+  '}'
 ];
 
 // Loop through each line of code
@@ -60,7 +60,6 @@ linesOfCode.forEach((line, index) => {
             const caretElement = element.querySelector('.Typewriter__cursor');
             caretElement.style.display = 'none';
           }, 0);
-          element.scrollIntoView({ behavior: 'smooth' });
         }, typewriter) // Pass the typewriter instance as the thisArg parameter
         .start(), 1000);
     }
@@ -97,8 +96,8 @@ delay(6000)
   .then(() => {
     oneLiner1.classList.remove("hidden");
     oneLiner1.classList.add("fade-in");
-    oneLiner2.scrollIntoView({ behavior: 'smooth' })
-    return delay(2500);
+    oneLiner1.scrollIntoView({ behavior: 'smooth' })
+    return delay(3000);
   })
   .then(() => {
     oneLiner2.classList.remove("hidden");
@@ -109,26 +108,139 @@ delay(6000)
   .then(() => {
     mainSection.classList.remove("hidden");
     mainSection.classList.add("fade-in");
-    mainSection.scrollIntoView({ behavior: 'smooth' })
+    mainSection.scrollIntoView({ behavior: 'smooth' });
+    document.body.style.height = '1000vh';
   });
+  
+window.addEventListener('scroll', function() {
+  let fixedDiv = document.getElementById('main-section');
+  let heroSection = document.getElementById('hero-section');
+  let aboutUs = document.getElementById('about-us');
+  let image = document.getElementById('image');
+  let aboutUsPara = document.getElementById('about-us-para');
+  const mediaQuerySmall = window.matchMedia("screen and (max-width: 700px)");
+  const mediaQueryMedium = window.matchMedia("screen and (min-width: 700px) and (max-width: 800px)");
+  let blur = 0;
 
-  window.addEventListener('scroll', function() {
-    let image = document.getElementById('image');
-    let num = (-image.getBoundingClientRect().y)/50;
-    let offset = image.getBoundingClientRect().top;
-    let blur = 0;
-    
-    console.log(num);
-    if (offset <= 0) {
-      blur = num;
-    } else {
-      blur = 0;
+  if (window.scrollY / 500 > 2) {
+    blur = ((window.scrollY / 500) - 2) * 10;       //Creating a number to control the overall behaviour of the components in the fixed div
+  } else {
+    blur = 0;
+  }
+  
+  if (heroSection.getBoundingClientRect().bottom + 25 < 0) {
+    fixedDiv.classList.add('fixed');        //Fixing the div so that it stays snapped to the viewport
+    if(mediaQuerySmall.matches){           //Executes only if the screen width is less than 700px
+      if(blur < 4){
+        aboutUs.style.opacity = blur/4;
+        aboutUs.style.transform = "translateY(" + - blur + "vh)";
+        aboutUs.style.transition = "transform";
+        aboutUs.style.transitionTimingFunction = "ease-in-out";
+
+        aboutUsPara.style.opacity = blur/4;
+        aboutUsPara.style.transform = "translateY(" + (blur) + "vh)";
+        aboutUsPara.style.transitionTimingFunction = "ease-in-out";
+        aboutUsPara.style.transition = "all";
+
+        image.style.filter = 'blur(' + blur + 'px)';
+
+        aboutUsPara.classList.remove("hidden");
+      } else if (blur > 12 && blur <= 20) {
+        aboutUs.style.opacity = 10/(blur);
+        aboutUs.style.transform = "translateY(" + - (blur - 8) + "vh)";
+        aboutUs.style.transitionTimingFunction = "ease-in-out";
+        aboutUs.style.transition = "all";
+
+        aboutUsPara.style.opacity = 10/(blur);
+        aboutUsPara.style.transform = "translateY(" + (blur - 8) + "vh)";
+        aboutUsPara.style.transitionTimingFunction = "ease-in-out";
+        aboutUsPara.style.transition = "all";
+
+        aboutUs.classList.remove("hidden");
+        aboutUsPara.classList.remove("hidden");
+      } else if (blur > 20) {
+        aboutUs.classList.add("hidden");
+        aboutUsPara.classList.add("hidden");
+      } else {
+        aboutUs.classList.remove('hidden');
+        aboutUsPara.classList.remove('hidden');
+      }
+    } else if (mediaQueryMedium.matches){               //Executes when Screen width is greater than 700px and less than 800px
+      if(blur < 4){
+        aboutUs.style.opacity = blur/4;
+        aboutUs.style.transform = "translateY(" + - blur * 2 + "vh)";
+        aboutUs.style.transition = "transform";
+        aboutUs.style.transitionTimingFunction = "ease-in-out";
+
+        aboutUsPara.style.opacity = blur/4;
+        aboutUsPara.style.transform = "translateY(" + (blur * 2) + "vh)";
+        aboutUsPara.style.transitionTimingFunction = "ease-in-out";
+        aboutUsPara.style.transition = "all";
+
+        image.style.filter = 'blur(' + blur + 'px)';
+
+        aboutUsPara.classList.remove("hidden");
+        aboutUsPara.style.opacity = blur/2;
+      } else if (blur > 12 && blur <= 14) {
+        aboutUs.style.opacity = 10/(blur);
+        aboutUs.style.transform = "translateY(" + - (blur - 8) * 2 + "vh)";
+        aboutUs.style.transitionTimingFunction = "ease-in-out";
+        aboutUs.style.transition = "all";
+
+        aboutUsPara.style.opacity = 10/(blur);
+        aboutUsPara.style.transform = "translateY(" + (blur - 8) * 2 + "vh)";
+        aboutUsPara.style.transitionTimingFunction = "ease-in-out";
+        aboutUsPara.style.transition = "all";
+      } else if (blur > 14) {
+        aboutUs.classList.add("hidden");
+      aboutUsPara.classList.add("hidden");
+      } else {
+        aboutUs.classList.remove('hidden');
+        aboutUsPara.classList.remove('hidden');
+      }
+    } else {                                              //Executes when the screen width is more than 800px
+      blur = (blur / 4) - 1.5;                            //Adjustifying the blur number because of the bigger screen which is why bigger scroll value is required
+      console.log(blur);
+      if(blur < 4){
+        aboutUs.style.opacity = blur/4;
+        aboutUs.style.transform = "translateY(" + - (blur * 3) + "vh)";
+        aboutUs.style.transition = "transform";
+        aboutUs.style.transitionTimingFunction = "ease-in-out";
+
+        aboutUsPara.style.opacity = blur/4;
+        aboutUsPara.style.transform = "translateY(" + (blur * 3) + "vh)";
+        aboutUsPara.style.transitionTimingFunction = "ease-in-out";
+        aboutUsPara.style.transition = "all";
+
+        image.style.filter = 'blur(' + blur + 'px)';
+
+        aboutUsPara.classList.remove("hidden");
+        aboutUs.classList.remove("hidden");
+      } else if (blur > 8 && blur <= 10) {
+        aboutUs.style.opacity = 10/(blur);
+        aboutUs.style.transform = "translateY(" + - (blur - 4) * 3 + "vh)";
+        aboutUs.style.transitionTimingFunction = "ease-in-out";
+        aboutUs.style.transition = "all";
+
+        aboutUsPara.style.opacity = 10/(blur);
+        aboutUsPara.style.transform = "translateY(" + (blur - 4) * 3 + "vh)";
+        aboutUsPara.style.transitionTimingFunction = "ease-in-out";
+        aboutUsPara.style.transition = "all";
+
+        aboutUs.classList.remove("hidden");
+        aboutUsPara.classList.remove("hidden");
+      } else if (blur > 10) {
+        aboutUs.classList.add("hidden");
+        aboutUsPara.classList.add("hidden");
+      } else {
+        aboutUs.classList.remove('hidden');
+        aboutUsPara.classList.remove('hidden');
+      }
     }
-  
-    image.style.filter = 'blur(' + blur + 'px)';
-  });
-  
-  
-  
+  } else if (heroSection.getBoundingClientRect().bottom >= 0) {
+    fixedDiv.classList.remove('fixed');
+    aboutUsPara.classList.add("hidden");
+    aboutUs.classList.add("hidden");
+  }
 
-var rellax = new Rellax('.rellax');
+});
