36323bed4b5c2ee99e724046b290f3c9d705bbe3
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-13 13:02:22 +0530

    Nirdesh Registration Page Complete

--- js/register.js
+++ js/register.js
@@ -1,1 +1,18 @@
 particlesJS("particles-js", particleConfig);
+
+const noticeModal = document.getElementById("notice-modal");
+const body = document.querySelector("body");
+
+if(!noticeModal.classList.contains("hidden")){
+          body.classList.add("overflow-hidden");
+}
+
+function hideModal(){
+      noticeModal.classList.add("hidden");
+      body.classList.remove("overflow-hidden");   
+}
+
+function showModal(){
+          noticeModal.classList.remove("hidden");   
+          body.classList.add("overflow-hidden"); 
+    }
