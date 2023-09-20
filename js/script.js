84aa2232eea161131f6937d7e485ced8761ca3c1
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-09-20 12:16:37 +0530

    session storage fixed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 particlesJS("particles-js", particleConfig);
 
 const loader = document.getElementById("loader");
+let visited = false;
 
-window.onload = function(){
-  loader.style.display = "none";
-}
+const nirdeshWriting = document.querySelector('#nirdesh-writing');
+const oneLiner1 = document.querySelector('#one-liner-1');
+const oneLiner2 = document.querySelector('#one-liner-2');
+const mainSection = document.querySelector('#fixed-div');
+const codeText = document.querySelectorAll(".code-text");
+const mediaQuerySmall = window.matchMedia("screen and (max-width: 700px)");
 
 const linesOfCode = [
   'package org.rkmvcc.nirdesh',
@@ -19,100 +23,114 @@ const linesOfCode = [
   '}'
 ];
 
-const codeText = document.querySelectorAll(".code-text");
+window.onload = function(){
+  loader.style.display = "none";
 
-// Loop through each line of code
-linesOfCode.forEach((line, index) => {
-    // Create a new typewriter instance for each line of code element
-
-  setTimeout(()=> {
-    const typewriter = new Typewriter(codeText[index], {
-      loop: false,
-      delay: 0,
+  if(sessionStorage.getItem('visited')){
+    linesOfCode.forEach ((line, index) => {
+      codeText[index].innerHTML = line;
     });
-
-    if(index==0){
-      typewriter
-        .typeString(line)
-        .pauseFor(1500)
-        .callFunction(() => {
-          setTimeout(() => {
-            typewriter.pause();
-            const caretElement = codeText[index].querySelector('.Typewriter__cursor');
-            caretElement.style.display = 'none';
-          }, 0);
-        }, typewriter) // Pass the typewriter instance as the thisArg parameter
-        .start();
-    } else if(index<5) {
-
-      setTimeout(() => typewriter
-        .typeString(line)
-        .pauseFor(500)
-        .callFunction(() => {
-          setTimeout(() => {
-            typewriter.pause();
-            const caretElement = codeText[index].querySelector('.Typewriter__cursor');
-            caretElement.style.display = 'none';
-          }, 0);
-        }, typewriter) // Pass the typewriter instance as the thisArg parameter
-        .start(), 500);
-        
-    } else {
-      
-      setTimeout(() => typewriter
-      .typeString(line)
-      .pauseFor(1000)
-      .callFunction(() => {
-        setTimeout(() => {
-          typewriter.pause();
-          const caretElement = codeText[index].querySelector('.Typewriter__cursor');
-          caretElement.style.display = 'none';
-        }, 0);
-      }, typewriter) // Pass the typewriter instance as the thisArg parameter
-      .start(), 1000);
-    }
-  }, (1000 * index)+1500);
-  
-  
-});
-
-
-const nirdeshWriting = document.querySelector('#nirdesh-writing');
-const oneLiner1 = document.querySelector('#one-liner-1');
-const oneLiner2 = document.querySelector('#one-liner-2');
-const mainSection = document.querySelector('#fixed-div');
-
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-delay(6000)
-  .then(() => {
     nirdeshWriting.classList.remove("hidden");
-    nirdeshWriting.classList.add("tracking-in-expand");
-    nirdeshWriting.scrollIntoView({ behavior: 'smooth' });
-    return delay(1000);
-  })
-  .then(() => {
     oneLiner1.classList.remove("hidden");
-    oneLiner1.classList.add("fade-in");
-    oneLiner1.scrollIntoView({ behavior: 'smooth' });
-    return delay(3000);
-  })
-  .then(() => {
     oneLiner2.classList.remove("hidden");
-    oneLiner2.classList.add("fade-in");
-    oneLiner2.scrollIntoView({ behavior: 'smooth' });
-  })
-  .then(() => {
     mainSection.classList.remove("hidden");
-    mainSection.classList.add("fade-in");
     if(mediaQuerySmall.matches){
       document.body.style.height = '700vh';
     } else {
       document.body.style.height = '1100vh';
     }
-  });
+  } else {
+      // Loop through each line of code
+    linesOfCode.forEach((line, index) => {
+      // Create a new typewriter instance for each line of code element
   
-const mediaQuerySmall = window.matchMedia("screen and (max-width: 700px)");
+    setTimeout(()=> {
+      const typewriter = new Typewriter(codeText[index], {
+        loop: false,
+        delay: 0,
+      });
+  
+      if(index==0){
+        typewriter
+          .typeString(line)
+          .pauseFor(1500)
+          .callFunction(() => {
+            setTimeout(() => {
+              typewriter.pause();
+              const caretElement = codeText[index].querySelector('.Typewriter__cursor');
+              caretElement.style.display = 'none';
+            }, 0);
+          }, typewriter) // Pass the typewriter instance as the thisArg parameter
+          .start();
+      } else if(index<5) {
+  
+        setTimeout(() => typewriter
+          .typeString(line)
+          .pauseFor(500)
+          .callFunction(() => {
+            setTimeout(() => {
+              typewriter.pause();
+              const caretElement = codeText[index].querySelector('.Typewriter__cursor');
+              caretElement.style.display = 'none';
+            }, 0);
+          }, typewriter) // Pass the typewriter instance as the thisArg parameter
+          .start(), 500);
+          
+      } else {
+        
+        setTimeout(() => typewriter
+        .typeString(line)
+        .pauseFor(1000)
+        .callFunction(() => {
+          setTimeout(() => {
+            typewriter.pause();
+            const caretElement = codeText[index].querySelector('.Typewriter__cursor');
+            caretElement.style.display = 'none';
+          }, 0);
+        }, typewriter) // Pass the typewriter instance as the thisArg parameter
+        .start(), 1000);
+      }
+    }, (1000 * index)+1500);
+  
+  
+    });
+  
+  
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+  
+    delay(6000)
+    .then(() => {
+      nirdeshWriting.classList.remove("hidden");
+      nirdeshWriting.classList.add("tracking-in-expand");
+      nirdeshWriting.scrollIntoView({ behavior: 'smooth' });
+      return delay(1000);
+    })
+    .then(() => {
+      oneLiner1.classList.remove("hidden");
+      oneLiner1.classList.add("fade-in");
+      oneLiner1.scrollIntoView({ behavior: 'smooth' });
+      return delay(3000);
+    })
+    .then(() => {
+      oneLiner2.classList.remove("hidden");
+      oneLiner2.classList.add("fade-in");
+      oneLiner2.scrollIntoView({ behavior: 'smooth' });
+    })
+    .then(() => {
+      mainSection.classList.remove("hidden");
+      mainSection.classList.add("fade-in");
+      if(mediaQuerySmall.matches){
+        document.body.style.height = '700vh';
+      } else {
+        document.body.style.height = '1100vh';
+      }
+    });
+  }
+
+  sessionStorage.setItem('visited', 'true');
+}
+
+
 
 window.addEventListener('scroll', function() {
   const fixedDiv = document.getElementById('fixed-div');
