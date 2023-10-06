03ea5760e5e57477219551527af2e2987ca447ea
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-06 08:27:37 +0530

    registration end page completed

--- js/register.js
+++ js/register.js
@@ -1,7 +1,54 @@
 particlesJS("particles-js", particleConfig);
 
-function openNirdesh(){
-	window.open("http://nirdesh.ilib.website/register/nirdesh-registration", "_self");
+function nirdeshTimer(tick) {
+	var counter = Tick.count.down('2023-10-07T09:30:00+05:30');
+
+	counter.onupdate = function(value) {
+	tick.value = value;
+	};
+}
+
+function eurekaTimer(tick) {
+	var counter = Tick.count.down('2023-10-07T12:00:00+05:30');
+
+	counter.onupdate = function(value) {
+	tick.value = value;
+	};
+}
+
+var nirdeshModal = document.getElementById("nirdesh-modal")
+var eurekaModal = document.getElementById("eureka-modal")
+
+function hideNirdeshModal(){
+	nirdeshModal.firstElementChild.classList.add("scale-down-center");
+	nirdeshModal.classList.add("blurred-out");
+	setTimeout(() => {
+		nirdeshModal.classList.add("hidden");
+		body.classList.remove("overflow-hidden");   
+	}, 200);
+}
+
+function openNirdeshModal(){
+	nirdeshModal.firstElementChild.classList.remove("scale-down-center");
+	nirdeshModal.classList.remove("blurred-out");
+	nirdeshModal.classList.remove("hidden");   
+	body.classList.add("overflow-hidden"); 
+}
+
+function hideEurekaModal(){
+	eurekaModal.firstElementChild.classList.add("scale-down-center");
+	eurekaModal.classList.add("blurred-out");
+	setTimeout(() => {
+		eurekaModal.classList.add("hidden");
+		body.classList.remove("overflow-hidden");   
+	}, 200);
+}
+
+function openEurekaModal(){
+	eurekaModal.firstElementChild.classList.remove("scale-down-center");
+	eurekaModal.classList.remove("blurred-out");
+	eurekaModal.classList.remove("hidden");   
+	body.classList.add("overflow-hidden"); 
 }
 
 function openEureka(){
