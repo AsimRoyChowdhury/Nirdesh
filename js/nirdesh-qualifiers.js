581e9d59d9dc586b7b8d06ba4a58ae7d46f9d57a
Asim Roy Chowdhury <asimrc2002@gmail.com>
2023-10-07 14:05:09 +0530

    added conferatti

--- js/nirdesh-qualifiers.js
+++ js/nirdesh-qualifiers.js
@@ -79,3 +79,33 @@ Object.values(qualifierDetails).map((val) => {
 	<div class="text-center my-auto font-[montserrat] text-white text-[12px] md:text-[25px]">${val.name}</div>
 	<hr class="col-span-2">`
 });
+
+
+var end = Date.now() + 3 * 1000;
+
+(function frame() {
+    confetti({
+        particleCount: 3,
+        angle: 60,
+        spread: 100,
+        origin: { x: 0 },
+        colors: ["#e54058", "#60a5fa", "#fff"],
+        zIndex: 10,
+        resize: true,
+        useWorker: true,
+    });
+    confetti({
+        particleCount: 3,
+        angle: 120,
+        spread: 100,
+        origin: { x: 1 },
+        colors: ["#e54058", "#60a5fa", "#fff"],
+        zIndex: 10,
+        resize: true,
+        useWorker: true,
+    });
+
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+    }
+})();
